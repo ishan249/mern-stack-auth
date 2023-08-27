9d936521ddafffb06856ba9f7151932a0c889fc7
ishan249 <249patelishan@gmail.com>
2023-08-27 15:09:55 +0530

    changed origins

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://mern-stack-auth-with-google-signin.vercel.app"],
     credentials: true,
   })
 );
