3b0bee690b4a252bb2b535ced084ff8f79ba435d
ishan249 <249patelishan@gmail.com>
2023-08-27 15:45:40 +0530

    checking /api

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", require("./routes/authRoutes"));
+app.use("/api", require("./routes/authRoutes"));
 const MONGOOSE_URL = process.env.MONGOOSE_URL;
 
 mongoose
